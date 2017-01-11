c21f736ced60dc45588aca043c73fc78e392696e
natali sol <natali.abyss@gmail.com>
2017-01-11 17:45:54 +0200

    docs(timepicker): create documentation for timepicker

--- demo/src/app/components/timepicker/demos/index.ts
+++ demo/src/app/components/timepicker/demos/index.ts
@@ -1,10 +1,25 @@
 import { TimepickerDemoComponent } from './timepicker-demo.component';
+import { DemoTimepickerBasicComponent } from './basic/basic';
+import { DemoTimepickerConfigComponent } from './config/config';
+import { DemoTimepickerMeridianComponent } from './meridian/meridian';
 
 export const DEMO_COMPONENTS = [
-  TimepickerDemoComponent
+  TimepickerDemoComponent, DemoTimepickerBasicComponent, DemoTimepickerConfigComponent, DemoTimepickerMeridianComponent
 ];
 
 export const DEMOS = {
+  basic: {
+    component: require('!!raw?lang=typescript!./basic/basic'),
+    html: require('!!raw?lang=markup!./basic/basic.html')
+  },
+  meridian: {
+    component: require('!!raw?lang=typescript!./meridian/meridian'),
+    html: require('!!raw?lang=markup!./meridian/meridian.html')
+  },
+  config: {
+    component: require('!!raw?lang=typescript!./config/config'),
+    html: require('!!raw?lang=markup!./config/config.html')
+  },
   old: {
     component: require('!!raw?lang=typescript!./timepicker-demo.component'),
     html: require('!!raw?lang=markup!./timepicker-demo.component.html')
